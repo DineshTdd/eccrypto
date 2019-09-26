ab8aec17dc415470b1f7bcd240e2dbe782d43b12
Dinesh Thirumaran <dineshthiru.1998@gmail.com>
2019-09-26 10:57:26 +0530

    update11

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/'));
+app.get('/', (req,res) => {
+    res.send('Hello world');
+    app.use(eci);
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4125);
