846f22803e9e68cebe9e3d18c7134c49ce423366
Dinesh Thirumaran <dineshthiru.1998@gmail.com>
2019-09-26 11:16:33 +0530

    update111

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const eccrypto = require("eccrypto");
 
 const app = express();
 
@@ -7,8 +8,30 @@ const app = express();
 app.use(express.static(__dirname + '/'));
 app.get('/', (req,res) => {
     res.send('Hello world');
-    app.use(eci);
+    // console.log('in ECC');
+ 
+  var privateKeyA = eccrypto.generatePrivate();
+  var publicKeyA = eccrypto.getPublic(privateKeyA);
+  var privateKeyB = eccrypto.generatePrivate();
+  var publicKeyB = eccrypto.getPublic(privateKeyB);
+   
+  // Encrypting the message for B.
+  eccrypto.encrypt(publicKeyB, Buffer.from("msg to b")).then(function(encrypted) {
+    // B decrypting the message.
+    eccrypto.decrypt(privateKeyB, encrypted).then(function(plaintext) {
+      res.send("Message to part B:", plaintext.toString());
+    });
+  });
+   
+  // Encrypting the message for A.
+  eccrypto.encrypt(publicKeyA, Buffer.from("msg to a")).then(function(encrypted) {
+    // A decrypting the message.
+    eccrypto.decrypt(privateKeyA, encrypted).then(function(plaintext) {
+      res.send("Message to part A:", plaintext.toString());
+    });
+  });
 });
+app.use(eci);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4125);
