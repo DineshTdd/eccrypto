cb86295431f2633bea5d93931da7b898e2d7b060
Dinesh Thirumaran <dineshthiru.1998@gmail.com>
2019-09-29 19:03:36 +0530

    eccrypto and cryptojs

--- ecies.js
+++ ecies.js
@@ -15,19 +15,7 @@ function eci (req, res, next) {
 
 const msg = "John 22 4432323";
 var result="";
- 
-// var jsonObj = JSON.parse(msg);
- 
-// // convert JSON object to String
-// var jsonStr = JSON.stringify(jsonObj , null, 4);
-
-// //var jsons = msg.toString();
-// //console.log(JSON.parse(msg).toString());
-// for (b in JSON.parse(msg)){
-//   console.log(JSON.parse(msg)[b]);
-// }
-// working string "'{\""+"name\""+":"+"john"+"}'"
-  eccrypto.encrypt(publicKeyB, "msg to a").then(function(encrypted) {
+  eccrypto.encrypt(publicKeyB,msg).then(function(encrypted) {
     // B decrypting the message.
     console.log(encrypted);
     eccrypto.decrypt(privateKeyB, encrypted).then(function(plaintext) {
@@ -41,35 +29,24 @@ var result="";
 
    
   // Encrypting the message for A.
-  eccrypto.encrypt(publicKeyA, Buffer.from("msg to b")).then(function(encrypted) {
+  eccrypto.encrypt(publicKeyA, Buffer.from(msg)).then(function(encrypted) {
     // A decrypting the message.
     eccrypto.decrypt(privateKeyA, encrypted).then(function(plaintext) {
       result+="</h1></div><div><h1>Message to part A:"+plaintext.toString()+'</h1></div>';
-      res.send('<div><h1>hello</h1></div><div><h1>'+result);
+      res.send('<div><h1>hello</h1></div><div><h1>Eccrypto working!</h1></div><div><h1>'+result+'<div><h1>CryptoJS working!</h1></div><div><h1>'+decryptedData+'</h1></div>');
     });
 
   });
 
-  var data = [{"id": "1","name":"john","card":"1234123412341234"},{"id": "1","name":"john","card":"1234123412341234"}]
- 
-  console.log((privateKeyA.toString('hex')+publicKeyA.toString('hex')+privateKeyB.toString()+publicKeyB.toString()).length);
-  console.log(publicKeyA.toString().length);
-  console.log(publicKeyB.toString().length);
-  console.log(privateKeyA.toString().length);
-  console.log(privateKeyB.toString().length);
+  var data = [{"id": "1","name":"john","card":"1234123412341234"}]
+
 // Encrypt
 var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), privateKeyA.toString('hex')+publicKeyA.toString('hex')+privateKeyB.toString()+publicKeyB.toString());
  
 // Decrypt
 var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), privateKeyA.toString('hex')+publicKeyA.toString('hex')+privateKeyB.toString()+publicKeyB.toString());
 var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
- 
-console.log(decryptedData);
 
-//res.send('Hello' + result);
-//console.log(result);
-//next();
 }
 
 module.exports = eci;
-// exports.result= this.result;
